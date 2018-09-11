5323fb8ac99f1803cf081677a365a9b577ddddeb
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-11 15:20:37 -0400

    convert from date-fns to moment, can increment by week and view current month based on week

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,43 +1,42 @@
 import React, { Component } from 'react';
-import dateFns from 'date-fns';
+import moment from 'moment';
 
 class Calendar extends Component {
     constructor(props) {
         super(props);
+        const now = moment();
+        const startOfWeek = now.startOf('week');
+        const endOfWeek = now.endOf('week');
         this.state = {
-            currentMonth: new Date(),
-            selectedDate: new Date()
+            now: now,
+            startOfWeek,
+            endOfWeek,
+            currentMonth: startOfWeek.format("MMMM YYYY")
         }
     }
     renderHeader() {
-        const dateFormat = "MMMM YYYY";
         return (
-          <div className="header row flex-middle">
-            <div className="col col-start">
-              <div className="icon" onClick={this.prevMonth}>
-                chevron_left
-              </div>
+            <div className="header row flex-middle">
+                <div className="col col-start">
+                    <div className="icon" onClick={this.prevWeek}>chevron_left</div>
+                </div>
+                <div className="col col-center">
+                    <span>{this.state.currentMonth}</span>
+                </div>
+                <div className="col col-end" onClick={this.nextWeek}>
+                    <div className="icon">chevron_right</div>
+                </div>
             </div>
-            <div className="col col-center">
-              <span>
-                {dateFns.format(this.state.currentMonth, dateFormat)}
-              </span>
-            </div>
-            <div className="col col-end" onClick={this.nextMonth}>
-              <div className="icon">chevron_right</div>
-            </div>
-          </div>
         );
-      }
+    }
 
     renderDays() {
-        const dateFormat = "dddd";
+        const weekdays = moment.weekdays();
         const days = [];
-        let startDate = dateFns.startOfWeek(this.state.currentMonth);
         for (let i = 0; i < 7; i++) {
           days.push(
             <div className="col col-center" key={i}>
-              {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+              {weekdays[i]}
             </div>
           );
         }
@@ -45,72 +44,34 @@ class Calendar extends Component {
     }
 
     renderCells() {
-        const { currentMonth, selectedDate } = this.state;
-        const monthStart = dateFns.startOfMonth(currentMonth);
-        const monthEnd = dateFns.endOfMonth(monthStart);
-        const startDate = dateFns.startOfWeek(monthStart);
-        const endDate = dateFns.endOfWeek(monthEnd);
-    
-        const dateFormat = "D";
-        const rows = [];
-    
-        let days = [];
-        let day = startDate;
-        let formattedDate = "";
-    
-        while (day <= endDate) {
-          for (let i = 0; i < 7; i++) {
-            formattedDate = dateFns.format(day, dateFormat);
-            const cloneDay = day;
-            days.push(
-              <div
-                className={`col cell ${
-                  !dateFns.isSameMonth(day, monthStart)
-                    ? "disabled"
-                    : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
-                }`}
-                key={day}
-                onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
-              >
-                <span className="number">{formattedDate}</span>
-                <span className="bg">{formattedDate}</span>
-              </div>
-            );
-            day = dateFns.addDays(day, 1);
-          }
-          rows.push(
-            <div className="row" key={day}>
-              {days}
-            </div>
-          );
-          days = [];
-        }
-        return <div className="body">{rows}</div>;
-      }
+
+    }
 
     onDateClick = day => {
-        this.setState({
-          selectedDate: day
-        });
-      };
+
+    };
   
-    nextMonth = () => {
+    nextWeek = () => {
         this.setState({
-          currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
-        });
-      };
-      prevMonth = () => {
+            startOfWeek: this.state.startOfWeek.add(1, 'week'),
+            endOfWeek: this.state.endOfWeek.add(1, 'week'),
+            currentMonth: this.state.startOfWeek.format("MMMM YYYY")
+        })
+    }
+    
+    prevWeek = () => {
         this.setState({
-          currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
-        });
-      };
+            startOfWeek: this.state.startOfWeek.subtract(1, 'week'),
+            endOfWeek: this.state.endOfWeek.subtract(1, 'week'),
+            currentMonth: this.state.startOfWeek.format("MMMM YYYY")
+        })
+    }
 
     render () {
         return (
             <div className="calendar">
                 {this.renderHeader()}
                 {this.renderDays()}
-                {this.renderCells()}
             </div>
         );
     }
