90b6fd091d9f0b09787af06f6586f41a01c93e6f
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-11 14:00:08 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React,  { Component } from 'react';
+import Calendar from "./Calendar";
+import '../css/App.css';
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <Calendar />
+            </div>
+        )
+    }
+}
+export default App;
